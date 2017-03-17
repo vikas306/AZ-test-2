97aeaba339d6eaebfbacf2b87793571b93493bfb
vikas306 <vikas.bakshi306@gmail.com>
2017-03-16 20:24:07 -0400

    AZ behavior
    Anxiety test

---
+++ AZ.R
@@ -0,0 +1,11 @@
+
+# AZ-practice-
+Practice
+
+# Definition of vectors
+Behavior_Test <- c("Anxiety")
+Parameters <- c("Closed arm duration", "Open arm duration", "Frequency Open arm", "Frequency Close arm")
+Mice <- c("Control", "Experimental")
+
+# Create a data frame from the vectors
+AZ_df <- data.frame(Behavior_Test, Parameters, Mice)
